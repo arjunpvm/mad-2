d53886cdc60df45154cb7d286ff92e2571f67c50
Arjun <arjungpvm@gmail.com>
2024-09-30 22:10:58 +0530

    temperature converter start

---
+++ basics/9.tempconvert/index.js
@@ -0,0 +1,10 @@
+function CtoF(celcius) {
+	let fahrenheit = Number(celcius) * 1.8 + 32;
+	return fahrenheit;
+}
+function FtoC(fahrenheit) {
+	let celcius = Number(fahrenheit) * 1.8 + 32;
+	return celcius;
+}
+
+
