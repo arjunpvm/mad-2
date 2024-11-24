c4b9a2e1e79fb094c576fe8c4ff852b5a9214703
Arjun <arjungpvm@gmail.com>
2024-11-24 11:14:02 +0530

    week 2 screencast completed

--- notes/w3/application.js
+++ notes/w3/application.js
@@ -1,6 +1,20 @@
 let app = new Vue({
     el: "#app",
     data: {
-        message: 'Lets learn Vue 2'
+        message: 'Lets learn Vue 2',
+        username: "",
+        visitors: []
     },
+    methods: {
+        letsGo: function() {
+            this.message = `Lets go motherfucker ${this.username}`;
+            this.visitors.push(this.username);
+            this.username = ""
+        },
+    },
+    computed: {
+        count: function() {
+            return this.visitors.length;
+        }
+    }
 })
